a58dccb1cd8db130cb703bd1b81f5956899b16e8
DUPIN <maximedupin1992@gmail.com>
2023-08-06 00:27:28 +0200

    remove token

--- bot/bot.go
+++ bot/bot.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/akhil/discord-ping/config"
 	"github.com/bwmarrin/discordgo"
@@ -42,8 +43,30 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == BotID {
 		return
 	}
-
-	if m.Content == "ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+	args := strings.Split(m.Content, " ")
+	if args[0] == config.BotPrefix {
+		return
 	}
+	
+	if m.Content == "hello" || m.Content == "Bonjour" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Hi, "+m.Author.Username+" I'm on 🔥 Fueg ")
+	}
+	prompt := m.Content[len(prefix):]
+
+	response, err := getOpenAIResponse(prompt)
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "Une erreur s'est produite lors de la communication avec l'API OpenAI.")
+		return
+	}
+
+	// Envoyez la réponse du modèle GPT-3.5 turbo sur le serveur Discord
+	s.ChannelMessageSend(m.ChannelID,  response)
+	
+}
+func getOpenAIResponse(prompt string) (string, error) {
+
+	// apiURL := 
+	// apiKey := 
+
+	return response, nil
 }
